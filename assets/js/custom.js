6436903171c611aaa840e33c5756f07c6aa45f05
yeasiniqra <yeasin@iqrasys.com>
2022-07-23 18:59:49 +0600

    checkout

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -266,6 +266,25 @@ function tabOpener(evt, kat) {
 }
 
 
+// modal js
+const modals = document.querySelectorAll("[data-modal]");
+
+modals.forEach(function (trigger) {
+  trigger.addEventListener("click", function (event) {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add("open");
+    const exits = modal.querySelectorAll(".modal-exit");
+    exits.forEach(function (exit) {
+      exit.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.classList.remove("open");
+      });
+    });
+  });
+});
+
+
 
 
 		
